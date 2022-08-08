1cee59e05d20116db6a870cd8e600c2792c551a4
jaldabaoth-code <grialazurabi@gmail.com>
2022-08-08 13:52:43 +0200

    fix navbar

--- public/includes/navbar.php
+++ public/includes/navbar.php
@@ -1,21 +1,25 @@
+<?php
+    
+$uri = $_SERVER['REQUEST_URI'];
+?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="http://localhost:8000">Home</a>
+                    <a class="nav-link <?= ($uri=='/index.php' || $uri=='/index' || $uri=='/') ? 'active' : '' ?>" aria-current="page" href="../">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="http://localhost:8000/heracles1/heracles1.php">Heracles 1</a>
+                    <a class="nav-link <?= ($uri=='/heracles1/heracles1.php') ? 'active' : '' ?>" href="../heracles1/heracles1.php">Heracles 1</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="http://localhost:8000/heracles2/heracles2.php">Heracles 2</a>
+                    <a class="nav-link <?= ($uri=='/heracles2/heracles2.php') ? 'active' : '' ?>" href="../heracles2/heracles2.php">Heracles 2</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="http://localhost:8000/heracles3/heracles3.php">Heracles 3</a>
+                    <a class="nav-link <?= ($uri=='/heracles3/heracles3.php') ? 'active' : '' ?>" href="../heracles3/heracles3.php">Heracles 3</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="http://localhost:8000/heracles4/heracles4.php">Heracles 4</a>
+                    <a class="nav-link <?= ($uri=='/heracles4/heracles4.php') ? 'active' : '' ?>" href="../heracles4/heracles4.php">Heracles 4</a>
                 </li>
             </ul>
         </div>
